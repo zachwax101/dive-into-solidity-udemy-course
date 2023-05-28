cfe35c152da36e93482c0476c736977b6a99cd8c
Zach <zachwax101@gmail.com>
2023-05-28 18:36:48 -0400

    knowledge test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,38 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+
+    constructor() {
+        owner = msg.sender;
     }
+
+    function changeTokens() public {
+        tokens[0] = "VET";
+    }
+
+    receive() external payable{
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address dest) public {
+        require(msg.sender == owner, "ONLY_OWNER");
+        payable(dest).transfer(address(this).balance);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata a, string calldata b) public pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
+    }
+
 }
+
+
+
